667e42ffc28391ae7b1068d678e12ebbcbcb5c66
neighborpil <neighborpil@naver.com>
2016-11-02 19:46:02 +0900

    SerializeExample
    C# TestingCodes to test github

---
+++ SerializeTest/SerializeTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerializeTest
+{
+    public class SampleSerializableClass
+    {
+        public string Value { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleSerializableClass obj = new SampleSerializableClass();
+            obj.Value = "Hello!";
+            XmlSerializer serializer = new XmlSerializer(typeof(SampleSerializableClass));
+
+            using(Stream stream = new FileStream("sample.xml", FileMode.Create))
+            {
+                serializer.Serialize(stream, obj);
+            }
+
+            using(Stream stream = new FileStream("sample.xml", FileMode.Open))
+            {
+                SampleSerializableClass read = (SampleSerializableClass)serializer.Deserialize(stream);
+                Console.WriteLine(read.Value);
+            }
+        }
+    }
+}
